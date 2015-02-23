8ab21dbc6960745899421d179f57ff23a85d11c2
Alex Browne <stephenalexbrowne@gmail.com>
2015-02-23 18:19:37 -0500

    Update copyright year to 2015 and add copyright notices.

--- doc.go
+++ doc.go
@@ -1,3 +1,7 @@
+// Copyright 2015 Alex Browne.  All rights reserved.
+// Use of this source code is governed by the MIT
+// license, which can be found in the LICENSE file.
+
 // package forms is a lightweight, but incredibly useful library for parsing
 // form data from an http.Request. It supports multipart forms, url-encoded
 // forms, json data, and url query parameters. It also provides helper methods
